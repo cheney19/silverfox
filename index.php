bc7b754c533efd51fe1d0cb53a49d23ce1b0aa03
cheney <upan2009@gmail.com>
2020-08-19 16:28:16 +0800

    修改library

--- index.php
+++ index.php
@@ -5,5 +5,19 @@
  * Date: 2020/8/11
  * Time: 2:19 PM
  */
-echo date();
+require_once './vendor/autoload.php'; // 加载自动加载文件
+//use AppleSignIn\ASDecoder;
+use Silverfox\Rose;
+/*use Silverfox\Rose\Rose;
+use Silverfox\Lily;
+
+$rose = new Rose();
+$lily = new Lily();
+
+$a = $rose->index();
+echo $a;*/
+
+$rose = new Rose();
+$a = $rose->index();
+echo $a;
 ?>
